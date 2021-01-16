bd56fc2a7367d08d67b651e641533c10b47ea918
Kiran Bhanushali <kiranbhanushli143.kb143@gmail.com>
2021-01-17 02:44:57 +0530

    auth reducer

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,75 +6,87 @@ var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 
 var passport = require("passport");
-require('../config/passport')(passport);
+require("../config/passport")(passport);
 var settings = require("../config/settings");
 
 exports.users = (req, res) => {
-    var users = User.find({}, function (err, users) {
-        res.send(users);
-    });
+  var users = User.find({}, function (err, users) {
+    res.send(users);
+  });
 };
 exports.register = (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        res.json({ success: false, msg: "Please pass username and password." });
-    } else {
-        var newUser = new User({
-            username: req.body.username,
-            password: req.body.password,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
+  if (!req.body.username || !req.body.password) {
+    res.json({ success: false, msg: "Please pass username and password." });
+  } else {
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+    });
+    // save the user
+    newUser.save(function (err) {
+      if (err) {
+        return res.json({
+          success: false,
+          msg: "Username already exists.",
+          err,
         });
-        // save the user
-        newUser.save(function (err) {
-            if (err) {
-                return res.json({
-                    success: false,
-                    msg: "Username already exists.",
-                    err,
-                });
-            }
-            res.json({ success: true, msg: "Successful created new user." });
-        });
-    }
+      }
+      res.json({
+        username: newUser.username,
+        firstname: newUser.firstname,
+        lastname: newUser.lastname,
+        email: newUser.email,
+        success: true,
+        msg: "Successful created new user.",
+      });
+    });
+  }
 };
 
 exports.login = (req, res) => {
-    User.findOne(
-        {
-            username: req.body.username,
-        },
-        function (err, user) {
-            if (err) throw err;
+  User.findOne(
+    {
+      username: req.body.username,
+    },
+    function (err, user) {
+      if (err) throw err;
 
-            if (!user) {
-                res.send({
-                    success: false,
-                    msg: "Authentication failed. User not found.",
-                });
-            } else {
-                // check if password matches
-                user.comparePassword(
-                    req.body.password,
-                    function (err, isMatch) {
-                        if (isMatch && !err) {
-                            // if user is found and password is right create a token
-                            var token = jwt.sign(
-                                user.toJSON(),
-                                settings.secret,
-                                { expiresIn: 86400 } // 24 hours
-                            );
-                            // return the information including token as JSON
-                            res.json({ success: true, token: "JWT " + token });
-                        } else {
-                            res.send({
-                                success: false,
-                                msg: "Authentication failed. Wrong password.",
-                            });
-                        }
-                    }
-                );
-            }
-        }
-    );
+      if (!user) {
+        res.send({
+          success: false,
+          msg: "Authentication failed. User not found.",
+        });
+      } else {
+        // check if password matches
+        user.comparePassword(req.body.password, function (err, isMatch) {
+          if (isMatch && !err) {
+            // if user is found and password is right create a token
+            var token = jwt.sign(
+              user.toJSON(),
+              settings.secret,
+              { expiresIn: 86400 } // 24 hours
+            );
+            // return the information including token as JSON
+            console.log(user);
+            res.json({
+              username: user.username,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              email: user.email,
+              success: true,
+              token: "JWT " + token,
+            });
+          } else {
+            res.send({
+              success: false,
+              msg: "Authentication failed. Wrong password.",
+            });
+          }
+        });
+      }
+    }
+  );
 };
