a7bf39bc5a26374db07b26e5433952291d558af8
Kiran Bhanushali <kiranbhanushli143.kb143@gmail.com>
2021-01-21 17:54:01 +0530

    dotenv for backend

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 export const BASE_API_URL = process.env.BACKEND_URL
     ? process.env.BACKEND_URL
-    : 'http://localhost/api/v1'
+    : 'http://localhost:5000/api/v1'
 
 axios.defaults.headers.common['authorization'] = localStorage.getItem('token')
 
