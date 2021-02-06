e37aa8bb15555ac1cc5abe94f6403125a1821ec0
jacker0031 <jenish31vekariy@gmail.com>
2021-02-06 11:37:03 +0530

    editor_set

--- frontend/src/components/Codemirror.js
+++ frontend/src/components/Codemirror.js
@@ -14,17 +14,25 @@ import 'codemirror/lib/codemirror.css'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/theme/material.css'
 import React from 'react'
+import 'codemirror/addon/display/autorefresh';
+import 'codemirror/addon/comment/comment';
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/keymap/sublime';
+import 'codemirror/mode/clike/clike';
+import 'codemirror/mode/python/python'
 import { Controlled as CodeMirror } from 'react-codemirror2'
+
 
 function CodeEditor(props) {
     const content = props.content
     const setContent = props.setContent
+    var mod = { "c": "text/x-csrc", "cpp": "text/x-c++src", "py": "python" };
     return (
         <CodeMirror
             style={{ alignItems: 'left', alignText: 'left' }}
             value={content.code}
             options={{
-                mode: content.language,
+                mode: mod[content.language],
                 theme: 'material',
                 lineWrapping: true,
                 smartIndent: true,
