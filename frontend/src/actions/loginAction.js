17e9a879b800dedca6e8cb32bd2955f7236956f4
Kiran Bhanushali <kiranbhanushli143.kb143@gmail.com>
2021-02-20 02:20:22 +0530

    submission succed

--- frontend/src/actions/loginAction.js
+++ frontend/src/actions/loginAction.js
@@ -3,23 +3,23 @@ import { fetchSubmission } from './fetchSubmission'
 import { loginError, loginRequest, loginSuccess } from './login'
 import { submissionFetch } from './submit'
 
-
 export async function loginF(dispatch, data) {
     dispatch(loginRequest())
-    await api.post(BASE_API_URL + '/auth/login', data)
+    await api
+        .post(BASE_API_URL + '/auth/login', data)
         .then(function (response) {
-            console.log("response", response)
+            console.log('response', response)
             let token = response.data.token
+            api.defaults.headers.common['authorization'] = token
             localStorage.setItem('token', token)
-
             dispatch(loginSuccess(token, response.data))
+            alert('Login Success')
         })
         .catch(function (error) {
             console.log('from erro')
             console.log(error.response)
             dispatch(loginError(error.response.data.msg))
         })
-    console.log("dispatch", dispatch)
-
+    console.log('dispatch', dispatch)
+    fetchSubmission(dispatch)
 }
-
