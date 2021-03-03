4910619a8cc409cf5ccca10d8bba0dd483924d54
Kiran Bhanushali <kiranbhanushali143.kb143@gmail.com>
2021-03-03 23:49:45 +0530

    total submission and accuracy count

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,109 +1,106 @@
-const config = require('../config/settings')
-const db = require('../models')
-const User = db.user
+const config = require("../config/settings");
+const db = require("../models");
+const User = db.user;
 
-var jwt = require('jsonwebtoken')
-var bcrypt = require('bcryptjs')
+var jwt = require("jsonwebtoken");
+var bcrypt = require("bcryptjs");
 
-var passport = require('passport')
-require('../config/passport')(passport)
-var settings = require('../config/settings')
+var passport = require("passport");
+require("../config/passport")(passport);
+var settings = require("../config/settings");
 
 exports.users = (req, res) => {
-    var users = User.find({}, function (err, users) {
-        res.send(users)
-    })
-}
+  var users = User.find({}, function (err, users) {
+    res.send(users);
+  });
+};
 exports.register = (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        res.status(404).json({
-            success: false,
-            msg: 'Please pass username and password.',
-        })
-    } else {
-        var newUser = new User({
-            username: req.body.username,
-            password: req.body.password,
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            email: req.body.email,
-            imageUrl: req.body.imageUrl,
-            isAdmin: req.body.isAdmin,
-        })
+  if (!req.body.username || !req.body.password) {
+    res.status(404).json({
+      success: false,
+      msg: "Please pass username and password.",
+    });
+  } else {
+    var newUser = new User({
+      username: req.body.username,
+      password: req.body.password,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      email: req.body.email,
+      imageUrl: req.body.imageUrl,
+      isAdmin: req.body.isAdmin,
+    });
 
-        // need to something for this
-        if (newUser.username === 'kkk1') newUser['isAdmin'] = true
-        // save the user
-        newUser.save(function (err) {
-            if (err) {
-                return res.status(409).json({
-                    success: false,
-                    msg: 'Username already exists.',
-                    err,
-                })
-            }
-            res.status(200).json({
-                username: newUser.username,
-                firstname: newUser.firstname,
-                lastname: newUser.lastname,
-                email: newUser.email,
-                imageUrl: newUser.imageUrl,
-                isAdmin: newUser.isAdmin,
+    // need to something for this
+    if (newUser.username === "kkk1") newUser["isAdmin"] = true;
+    // save the user
+    newUser.save(function (err) {
+      if (err) {
+        return res.status(409).json({
+          success: false,
+          msg: "Username already exists.",
+          err,
+        });
+      }
+      res.status(200).json({
+        username: newUser.username,
+        firstname: newUser.firstname,
+        lastname: newUser.lastname,
+        email: newUser.email,
+        imageUrl: newUser.imageUrl,
+        isAdmin: newUser.isAdmin,
 
-                success: true,
-                msg: 'Successful created new user.',
-            })
-        })
-    }
-}
+        success: true,
+        msg: "Successful created new user.",
+      });
+    });
+  }
+};
 
 exports.login = (req, res) => {
-    User.findOne(
-        {
-            username: req.body.username,
-        },
-        function (err, user) {
-            if (err) throw err
+  User.findOne(
+    {
+      username: req.body.username,
+    },
+    function (err, user) {
+      if (err) throw err;
 
-            if (!user) {
-                res.status(404).send({
-                    success: false,
-                    msg: 'Authentication failed. User not found.',
-                })
-            } else {
-                // check if password matches
-                user.comparePassword(
-                    req.body.password,
-                    function (err, isMatch) {
-                        if (isMatch && !err) {
-                            // if user is found and password is right create a token
-                            var token = jwt.sign(
-                                user.toJSON(),
-                                settings.secret,
-                                { expiresIn: 86400 } // 24 hours
-                            )
-                            // return the information including token as JSON
-                            console.log(user)
-                            res.status(200).json({
-                                username: user.username,
-                                firstname: user.firstname,
-                                lastname: user.lastname,
-                                email: user.email,
-                                isAdmin: user.isAdmin,
-                                success: true,
-                                token: 'JWT ' + token,
-                                msg: 'login successful',
-                                imageUrl: user.imageUrl,
-                            })
-                        } else {
-                            res.status(403).send({
-                                success: false,
-                                msg: 'Authentication failed. Wrong password.',
-                            })
-                        }
-                    }
-                )
-            }
-        }
-    )
-}
+      if (!user) {
+        res.status(404).send({
+          success: false,
+          msg: "Authentication failed. User not found.",
+        });
+      } else {
+        // check if password matches
+        user.comparePassword(req.body.password, function (err, isMatch) {
+          if (isMatch && !err) {
+            // if user is found and password is right create a token
+            var token = jwt.sign(
+              user.toJSON(),
+              settings.secret,
+              { expiresIn: 86400 } // 24 hours
+            );
+            // return the information including token as JSON
+            console.log(user);
+            res.status(200).json({
+              username: user.username,
+              firstname: user.firstname,
+              lastname: user.lastname,
+              email: user.email,
+              isAdmin: user.isAdmin,
+              success: true,
+              token: "JWT " + token,
+              msg: "login successful",
+              imageUrl: user.imageUrl,
+            });
+          } else {
+            res.status(403).send({
+              success: false,
+              msg: "Authentication failed. Wrong password.",
+            });
+          }
+        });
+      }
+    }
+  );
+};
--- frontend/src/reducers/auth.js
+++ frontend/src/reducers/auth.js
@@ -1,119 +1,113 @@
 const initialState = {
-    imageUrl: null,
-    username: null,
-    firstname: null,
-    lastname: null,
-    email: null,
-    isLoggedIn: false,
-    error: { login: null, register: null, uploadImage: null },
-    message: null,
-    token: null,
-    pending: false,
-    isAdmin: false,
-}
+  imageUrl: null,
+  username: null,
+  firstname: null,
+  lastname: null,
+  email: null,
+  isLoggedIn: false,
+  error: { login: null, register: null, uploadImage: null },
+  message: null,
+  token: null,
+  pending: false,
+  isAdmin: false,
+};
 
 export default function authReducer(state = initialState, action) {
-    switch (action.type) {
-        case 'LOGIN_REQUEST':
-            return {
-                ...state,
-                pending: true,
-            }
-        case 'LOGIN_SUCCESS':
-            return {
-                ...state,
-                token: action.token,
-                username: action.username,
-                firstname: action.firstname,
-                lastname: action.lastname,
-                email: action.email,
-                isLoggedIn: true,
-                message: action.msg,
-                error: null,
-                pending: false,
-                imageUrl: action.imageUrl,
-                isAdmin: action.isAdmin,
-
-            }
-        case 'LOGIN_ERROR':
-            return {
-                ...state,
-                error: {
-                    login: action.error,
-                    register: state?.error?.register,
-                },
-                pending: false,
-            }
-        case 'LOGOUT':
-            return {
-                ...state,
-                username: null,
-                firstname: action.firstname,
-                lastname: action.lastname,
-                email: action.email,
-                token: null,
-                isLoggedIn: false,
-                message: null,
-                error: null,
-                pending: false,
-                isAdmin: false,
-            }
-        case 'REGISTER_REQUEST':
-            return {
-                ...state,
-                pending: true,
-            }
-        case 'REGISTER_SUCCESS':
-            return {
-                ...state,
-                token: action.token,
-                username: action.username,
-                isLoggedIn: true,
-                message: action.msg,
-                error: null,
-                pending: false,
-            }
-        case 'REGISTER_ERROR':
-            return {
-                ...state,
-                error: {
-                    login: state?.error?.login,
-                    register: action.error,
-                },
-                pending: false,
-            }
-        case 'CLEAR_AUTH_ERROR':
-            return {
-                ...state,
-                error: null,
-            }
-        case 'UPLOAD_IMAGE_PENDING':
-            return {
-                ...state,
-                pending: true,
-            }
-        case 'UPLOAD_IMAGE_ERROR':
-            return {
-                ...state,
-                error: {
-                    login: state?.error?.login,
-                    register: state?.error?.register,
-                    uploadImage: action.payload
-                },
-                pending: false,
-            }
-        case 'UPLOAD_IMAGE_SUCCESS':
-            return {
-                ...state,
-                imageUrl: action.payload,
-                pending: false
-
-            }
-        default:
-            return state
-
-    }
+  switch (action.type) {
+    case "LOGIN_REQUEST":
+      return {
+        ...state,
+        pending: true,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        token: action.token,
+        username: action.username,
+        firstname: action.firstname,
+        lastname: action.lastname,
+        email: action.email,
+        isLoggedIn: true,
+        message: action.msg,
+        error: null,
+        pending: false,
+        imageUrl: action.imageUrl,
+        isAdmin: action.isAdmin,
+      };
+    case "LOGIN_ERROR":
+      return {
+        ...state,
+        error: {
+          login: action.error,
+          register: state?.error?.register,
+        },
+        pending: false,
+      };
+    case "LOGOUT":
+      return {
+        ...state,
+        username: null,
+        firstname: action.firstname,
+        lastname: action.lastname,
+        email: action.email,
+        token: null,
+        isLoggedIn: false,
+        message: null,
+        error: null,
+        pending: false,
+        isAdmin: false,
+      };
+    case "REGISTER_REQUEST":
+      return {
+        ...state,
+        pending: true,
+      };
+    case "REGISTER_SUCCESS":
+      return {
+        ...state,
+        token: action.token,
+        username: action.username,
+        isLoggedIn: true,
+        message: action.msg,
+        error: null,
+        pending: false,
+      };
+    case "REGISTER_ERROR":
+      return {
+        ...state,
+        error: {
+          login: state?.error?.login,
+          register: action.error,
+        },
+        pending: false,
+      };
+    case "CLEAR_AUTH_ERROR":
+      return {
+        ...state,
+        error: null,
+      };
+    case "UPLOAD_IMAGE_PENDING":
+      return {
+        ...state,
+        pending: true,
+      };
+    case "UPLOAD_IMAGE_ERROR":
+      return {
+        ...state,
+        error: {
+          login: state?.error?.login,
+          register: state?.error?.register,
+          uploadImage: action.payload,
+        },
+        pending: false,
+      };
+    case "UPLOAD_IMAGE_SUCCESS":
+      return {
+        ...state,
+        imageUrl: action.payload,
+        pending: false,
+      };
+    default:
+      return state;
+  }
 }
-
-
-
